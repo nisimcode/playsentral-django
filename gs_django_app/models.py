74033d1268e7372923babe352cc611b5632835cd
nisimgit <nisimlevi@ymail.com>
2022-03-15 21:34:57 +0200

    v6 - refined models.py

--- gs_django_app/models.py
+++ gs_django_app/models.py
@@ -33,7 +33,7 @@ class Company(models.Model):
 #         return self.name
 #
 
-# # Genre is at this point a single field of Game, may yet change it to two fields or model.
+# # Genre is at this point two fields of Game, may yet change it to a model.
 
 # class Genre(models.Model):
 #     GENRES = (
@@ -50,9 +50,7 @@ class Company(models.Model):
 #     def __str__(self):
 #         return self.name
 
-# # Series class is possibly useful for games that are connected to each other.
-# # I will have to look into having a foreign key that can have null value,
-# # since not all games belong to a series. Is it ok? Probably will be just fine and be used.
+# # Games related to each other will share series, like FIFA games.
 
 class Series(models.Model):
     name = models.CharField(max_length=128)
@@ -81,14 +79,11 @@ class Game(models.Model):
     ratings = models.ManyToManyField(User, related_name='games_rated', through='Rating')
     threads = models.ManyToManyField(User, related_name='games_threaded', through='Thread')
     is_deleted = models.BooleanField(default=False)
-
     genre_1 = models.CharField(max_length=128, choices=GENRES)
     genre_2 = models.CharField(max_length=128, choices=GENRES, null=True, blank=True)
     # OR
-    # genre = models.ForeignKey(Genre, on_delete=models.PROTECT, related_name='games')
-
-    # platforms = models.ManyToManyField(Platform, on_delete=models.PROTECT, related_name='games',
-    #                                    through='GameVersion', null=True, blank=True)
+    # genre = models.ManyToManyField(Genre, through='GameGenre')
+    # platforms = models.ManyToManyField(Platform, through='PlatformGame')
 
     class Meta:
         db_table = "games"
@@ -96,15 +91,28 @@ class Game(models.Model):
     def __str__(self):
         return self.name
 
-# # GameVersion class will probably be created if and when I decide to use a Platform model and
-# # thus create M2M relationship between Game and Platform through this model. Undecided yet.
 
-# class GameVersion(models.Model):
-#     game = models.ForeignKey(Game, on_delete=models.PROTECT, related_name='game_versions')
-#     platform = models.ForeignKey(Platform, on_delete=models.PROTECT, related_name='game_versions')
+# # If genre becomes a model, Game and Genre will have M2M through GameGenre
+
+# class GameGenre(models.Model):
+#     game = models.ForeignKey(Game, on_delete=models.PROTECT)
+#     genre = models.ForeignKey(Genre, on_delete=models.PROTECT)
 #
 #     class Meta:
 #         db_table = "game_versions"
+#
+#     def __str__(self):
+#         return f'{self.game}, {self.platform}'
+
+# # PlatformGame class will probably be created if and when I decide to use a Platform model and
+# # thus create M2M relationship between Game and Platform through this model. Undecided yet.
+
+# class PlatformGame(models.Model):
+#     game = models.ForeignKey(Game, on_delete=models.PROTECT)
+#     platform = models.ForeignKey(Platform, on_delete=models.PROTECT)
+#
+#     class Meta:
+#         db_table = "platform_games"
 #
 #     def __str__(self):
 #         return f'{self.game}, {self.platform}'
@@ -113,7 +121,7 @@ class Game(models.Model):
 # # Registered users can rate a game, and remove and change that rating. One instance per user.
 
 class Rating(models.Model):
-    RATINGS = ((1, 1), (2, 2), (3, 3), (4, 4), (5, 5))
+    RATINGS = ((1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10))
 
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     game = models.ForeignKey(Game, on_delete=models.PROTECT)
@@ -137,11 +145,10 @@ class Thread(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     comments = models.ManyToManyField(User, related_name='threads', through='Comment')
     is_deleted = models.BooleanField(default=False)
+    is_closed = models.BooleanField(default=False)
 
     # # Will look into how to implement is_closed, probably through a simple serializer, starter/superuser only
     # # Will prevent posting of comments to this thread when True
-
-    # is_closed = models.BooleanField(default=False)
 
     class Meta:
         db_table = "threads"
@@ -159,6 +166,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
+    text = models.TextField(max_length=512)
 
     class Meta:
         db_table = "comments"
