a0138bcbbca866cd1f75185da5d8ac0e734af701
nisimgit <nisimlevi@ymail.com>
2022-04-13 20:25:34 +0300

    Added files and changes required for deployment to heroku

--- gs_django/settings.py
+++ gs_django/settings.py
@@ -13,6 +13,11 @@ import os
 from pathlib import Path
 import dj_database_url
 import django_heroku
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-with open('../secret/secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = env('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
