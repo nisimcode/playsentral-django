943b6d65a63534d3afce220514b09f4fc567a374
nisimgit <nisimlevi@ymail.com>
2022-05-05 00:14:11 +0300

    again

--- gs_django/settings.py
+++ gs_django/settings.py
@@ -30,7 +30,7 @@ DEBUG = False
 
 # ALLOWED_HOSTS = ['127.0.0.1']
 
-ALLOWED_HOSTS = ['playsentral-django.herokuapp.com']
+ALLOWED_HOSTS = ['playsentral-django.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
