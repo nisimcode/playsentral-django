4feeed2100ae826f52899cbcd9c13c35409195a8
nisimgit <nisimlevi@ymail.com>
2022-05-04 23:27:48 +0300

    ---

--- gs_django/settings.py
+++ gs_django/settings.py
@@ -26,11 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['127.0.0.1']
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://playsentral-django.herokuapp.com/']
+ALLOWED_HOSTS = ['playsentral-django.herokuapp.com']
 
 # Application definition
 
