d317a7e746d5edfb1e52a8f0043e340e2d8d9fa7
nisimgit <nisimlevi@ymail.com>
2022-05-06 23:33:16 +0300

    secretkey

--- gs_django/settings.py
+++ gs_django/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-# SECRET_KEY = '1v&-j(h6kw=2d+xv1zc6hqy2$6l!r%dnphckzb7!^psk2x44ia'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
